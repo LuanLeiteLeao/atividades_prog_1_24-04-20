712bb28e62b2bf1b364ac817ef6746f1741f86ce
dudaf <dudaf@LAPTOP-9QQNKIPG>
2020-04-25 10:24:32 -0300

    Exercício 02

--- src/exerc02/Piramide.java
+++ src/exerc02/Piramide.java
@@ -2,35 +2,38 @@ package exerc02;
 
 public class Piramide {
 	
+	private double base;
+	private double altura;
+
 	public Piramide(double base, double altura) {
 		super();
 		this.base = base;
 		this.altura = altura;
 	}
 
-	private double base;
-	private double altura;
-	
 	public double getBase() {
 		return base;
 	}
+
 	public void setBase(double base) {
 		this.base = base;
 	}
+
 	public double getAltura() {
 		return altura;
 	}
+
 	public void setAltura(double altura) {
 		this.altura = altura;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Piramide [base= " + base + ", altura= " + altura + "]";
 	}
-		
-		public double calculaVolume() {
-			
-			return (this.getBase() * this.getAltura() )*1/3;
-		}
+
+	public double calculaVolume() {
+
+		return (this.getBase() * this.getAltura()) * 1 / 3;
 	}
+}
