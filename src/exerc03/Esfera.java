77eb92ccfcbc2a0fc4415fd8ee23bcb88d35167e
dudaf <dudaf@LAPTOP-9QQNKIPG>
2020-04-25 10:23:02 -0300

    Exercício 03 (Atualizado)

--- src/exerc03/Esfera.java
+++ src/exerc03/Esfera.java
@@ -1,15 +1,16 @@
 package exerc03;
 
+import javax.management.RuntimeErrorException;
+
 public class Esfera {
-	
+
 	private double raio;
-	
-	/**
-	 * @param raio
-	 */
+
 	public Esfera(double raio) {
 		super();
-		this.raio = raio;
+
+		this.setRaio(raio);
+
 	}
 
 	public double getRaio() {
@@ -17,16 +18,21 @@ public class Esfera {
 	}
 
 	public void setRaio(double raio) {
-		this.raio = raio;
+		
+		if (raio > 0) {
+			this.raio = raio;
+		} else {
+			System.out.println("\nNúmero inválido!");
+			System.exit(1);
+		}
 	}
-	
-	public double areaTotalEsfera()
-	{
+
+	public double areaTotalEsfera() {
 		return 4 * 3.1415 * Math.pow(this.getRaio(), 2);
 	}
-	
+
 	public double volumeEsfera() {
-		
-		return 4/3 * (3.1415 * Math.pow(this.getRaio(), 3));
+		return 4 / 3 * (3.1415 * Math.pow(this.getRaio(), 3));
 	}
+
 }
--- src/exerc03/TesteEsfera.java
+++ src/exerc03/TesteEsfera.java
@@ -3,12 +3,12 @@ package exerc03;
 public class TesteEsfera {
 
 	public static void main(String[] args) {
-		
+
 		Esfera valor = new Esfera(6);
-		
+
 		System.out.printf("Área da esfera: %.2fm²", valor.areaTotalEsfera());
 		System.out.printf("\n\nVolume da esfera: %.2fm³", valor.volumeEsfera());
-
+		
 	}
 
 }
